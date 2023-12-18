28bc6be0af9a75be9a036bf3fe05436ac760ec28
SUDHANSHU <gusainsudhanshu43@gmail.com>
2023-12-18 17:29:21 +0530

    exception and logging

--- src/logger.py
+++ src/logger.py
@@ -15,6 +15,3 @@ logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s"
 )
-
-if __name__ == "__main__":
-    logging.info("Testing the logger")
